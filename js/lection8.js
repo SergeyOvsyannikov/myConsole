ef2fb601dd554039b63e1602e65eb4205d34b80c
SergeyOvsyannikov <turboman007@inbox.ru>
2018-11-29 13:32:16 +0300

    log

---
+++ js/lection8.js
@@ -0,0 +1,25 @@
+/*
+
+xmlhttprequest(??);
+глобальный объект Proxy;
+observable js;
+Филипп Робертс: event loop;
+singletone(???);
+самовызываемая функция(???);
+
+* */
+
+/*var getData = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({name:"Ilya"});
+    }, 3000);
+});
+
+getData.then(function(data){
+    console.log("ok");
+    console.dir(data);
+}, function(){
+    console.log('not ok');
+})*/
+
+/*!!!!ДЗ!!!! СВОЙ PROMISE*/
---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+/*const myConsole = {};
+myConsole.__proto__ = console;
+myConsole.log=function(arg){
+    this.__proto__.log(arg);
+    localStorage.setItem('fromConsole', arg);
+};
+console=myConsole;*/
+
+let tmp;
+tmp = window.console;
+const myConsole = {};
+myConsole.log = function(){
+    let join = [].join;
+    tmp.log(...arguments);
+    localStorage.setItem('fromConsole', join.call(arguments, "; ") );
+
+}
+
+console=myConsole;
+
+console.log("1", window);
